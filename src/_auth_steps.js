94a7d95383ba7b45fc3eb8b28409744cece35224
arfat-beep <arfatrahman08@gmail.com>
2022-04-12 17:24:14 +0600

    final

--- src/_auth_steps.js
+++ src/_auth_steps.js
@@ -15,8 +15,9 @@
 
 /* 
     Hosting steps
-    1.  npm install -g firebase-tools
-    2.  firebase login
-    3.  firebase init
-    4.  firebase deploy
+    1.  npm install -g firebase-tools (one timie for your pc)
+    2.  firebase login (one timie for your pc)
+    3.  firebase init (once for each project)
+    4.  npm run build (every time you want to deploy)
+    5.  firebase deploy (every time you want to deploy)
 */
